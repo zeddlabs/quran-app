3d53c23c5124bbb9bc5bc75a66e3504e6a0057b5
Mhd. Zulfikar Pinem <zulfikarm022@gmail.com>
2023-09-20 08:00:27 +0700

    init: initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# quran_app
+
+A new Flutter project.
---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:quran_app/screens/main_screen.dart';
+import 'package:quran_app/screens/onboarding.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+int? hasSeenOnboarding;
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  hasSeenOnboarding = await prefs.getInt('hasSeenOnboarding');
+  await prefs.setInt('hasSeenOnboarding', 1);
+
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: hasSeenOnboarding == 0 || hasSeenOnboarding == null
+          ? const OnboardingScreen()
+          : const MainScreen(),
+    );
+  }
+}
